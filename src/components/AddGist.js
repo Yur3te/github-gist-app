6499eb2cbded9918e695285ceb829204a7091eab
Yur3te <Yur3te@gmail.com>
2021-08-20 16:47:17 +0200

    some code format

--- src/components/AddGist.js
+++ src/components/AddGist.js
@@ -1,81 +1,52 @@
-import React, { useState } from 'react';
-import { useRef } from 'react'
+import React, { useState, useRef } from "react";
 
 import gistsWrapper from "../js/gistsWrapper";
-const token = require("../js/config.js")
+const token = require("../js/config.js");
 
+export default function AddGist() {
+  const getWrapper = function () {
+    const wrapper = new gistsWrapper(token);
+    return wrapper;
+  };
 
+  const fileNameRef = useRef();
 
-function AddGist(props) {
+  const create = (event) => {
+    const enteredFileName = fileNameRef.current.value;
+    event.preventDefault();
 
-    const getWrapper = function() {
-        const wrapper = new gistsWrapper(token)
-        return wrapper
-    }
+    const file = { name: enteredFileName, content: "test" };
 
-    const fileNameRef = useRef();
+    let gistPayload = {
+      description: "opis",
+      public: true,
+      files: { file },
+    };
 
+    const wrapper = getWrapper();
+    wrapper
+      .createGist(gistPayload)
+      .then((response) => console.log(response.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    // const [files, setFiles] = useState()
-
-    // const filenameHandler = (value, i) =>
-    //   setFiles(files => files.map((item, index) => index === i ? {...item, name: value} : item))
-
-
-
-
-
-
-     const create = (event) => {
-        const enteredFileName = fileNameRef.current.value;
-        event.preventDefault();
-
-
-
-        
-
-        let gistPayload = {
-          "description": "opis",
-          "public": true,
-          "files": {
-            enteredFileName: {
-            "content": "nazwa",
-            }
-          }
-        }
-
-        // let gistPayload = {
-        //   "description": "opis",
-        //   "public": true,
-        //   "files": {
-        //     "czy dziala.txt": {
-        //       "content": "ni chuja :D"
-        //     }
-        //   }
-        // }
-
-        const wrapper = getWrapper()
-        wrapper.createGist(gistPayload).then((response) => console.log(response.data)).catch((err) => {console.log(err)})
-    }
-
-
-    return(
+  return (
     <div>
-        <div>
-            <form onSubmit={create}>
-                <div>
-                    <label>Nazwa pliku:</label>
-                    <input type="text" ref={fileNameRef}/>
-                </div>
-                <div>
-                    <label>Treść:</label>
-                    <input type="text"/>
-                </div>
-                <input type="submit" value="Wyślij" />
+      <div>
+        <form onSubmit={create}>
+          <div>
+            <label>Nazwa pliku:</label>
+            <input type="text" ref={fileNameRef} />
+          </div>
+          <div>
+            <label>Treść:</label>
+            <input type="text" />
+          </div>
+          <input type="submit" value="Wyślij" />
         </form>
       </div>
     </div>
-    )
+  );
 }
-
-export default AddGist
