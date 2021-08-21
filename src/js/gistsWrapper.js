eba4f1e3f2b54702bdca723b7a41cddf447a14f9
Yur3te <Yur3te@gmail.com>
2021-08-21 23:05:24 +0200

    changed some requests

--- src/js/gistsWrapper.js
+++ src/js/gistsWrapper.js
@@ -1,65 +1,67 @@
-const axios = require('axios');
-const token = require("./config.js")
-
-
+const axios = require("axios");
+const token = require("./config.js");
 
 class gistsWrapper {
-    constructor(token) {
-      this.token = token
-      this.client = axios.create({
-        baseURL: 'https://api.github.com/',
-        responseType: 'json',
-        headers: {
-          'Accept': 'application/vnd.github.v3+json',
-          'Authorization': 'token ' + this.token
-        }
-      })
-    }
+  constructor(token) {
+    this.token = token;
+    this.client = axios.create({
+      baseURL: "https://api.github.com/",
+      responseType: "json",
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: "token " + this.token,
+      },
+    });
+  }
 
-      getRequest(path) {
-        return this.client.get(path)
-      }
+  getRequest(path) {
+    return this.client.get(path);
+  }
 
-      postRequest(path, payload) {
-        return this.client.post(path, payload)
-      }
-      
-      createGist(payload) {
-        return this.postRequest('/gists', payload)
-      }
-    
+  deleteRequest(path) {
+    return this.client.delete(path);
+  }
 
-      root() {
-        return this.getRequest('/')
-      }
-    
-      getGist(gistId) {
-        return this.getRequest(`/gists/${gistId}`)
-      }
+  postRequest(path, payload) {
+    return this.client.post(path, payload);
+  }
 
-      getAllGists() {
-        return this.getRequest('/gists')
-      }
-    }
+  root() {
+    return this.getRequest("/");
+  }
 
+  createGist(payload) {
+    return this.postRequest("/gists", payload);
+  }
 
-    // let ghWrapper = new gistsWrapper(token)
-    // let gistPayload = {
-    //   "description": "test",
-    //   "public": true,
-    //   "files": {
-    //     "czy dziala.txt": {
-    //       "content": "ni chuja :D"
-    //     }
-    //   }
-    // }
-    
-    
-    //ghWrapper.root().then((response) => console.log(response.data))
-    //ghWrapper.getGist('4840102b0553184fb6853fb70a6b49ff').then((response) => console.log(response.data))
-    //ghWrapper.createGist(gistPayload).then((response) => console.log(response.data))
-    //ghWrapper.getRequest('/gists').then((response) => console.log(response.data))
-    //ghWrapper.validation().then((response) => console.log(response.data))
+  getGist(gistId) {
+    return this.getRequest(`/gists/${gistId}`);
+  }
 
-    export default gistsWrapper
+  deleteGist(gistId) {
+    return this.deleteRequest(`/gists/${gistId}`);
+  }
 
+  getAllGists() {
+    return this.getRequest("/gists");
+  }
+}
+
+// let ghWrapper = new gistsWrapper(token)
+// let gistPayload = {
+//   "description": "test",
+//   "public": true,
+//   "files": {
+//     "czy dziala.txt": {
+//       "content": "ni chuja :D"
+//     }
+//   }
+// }
+
+//ghWrapper.root().then((response) => console.log(response.data))
+//ghWrapper.getGist('4840102b0553184fb6853fb70a6b49ff').then((response) => console.log(response.data))
+//ghWrapper.createGist(gistPayload).then((response) => console.log(response.data))
+//ghWrapper.getRequest('/gists').then((response) => console.log(response.data))
+//ghWrapper.validation().then((response) => console.log(response.data))
+
+export default gistsWrapper;
