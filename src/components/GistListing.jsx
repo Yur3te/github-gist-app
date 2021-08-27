644e8d60ac85fdee41ea312f8a5fc2c3e430213e
Yur3te <Yur3te@gmail.com>
2021-08-27 14:15:04 +0200

    made gist listing possible

--- src/components/GistListing.jsx
+++ src/components/GistListing.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 export default function GistListing(props) {
   const [gists, setGists] = useState([]);
+
+
+
+  const deleteGist = (id) => {
+    props.wrapper.deleteGist(id)
+    .then(response => {
+      console.log(response.setatus)
+        // if (response.status === 204) {
+        //     console.log("gist został usunięty", id)
+        // }
+    })
+}
+
   
   const getListOfGists = () => {
     props.wrapper
       .getAllGists()
-      .then((response) => setGists(response.data))
+      .then((response) => {
+        setGists(response.data)
+      })
+      .catch(() => [])
       
       // .catch(() => []);
     // setGists(res);
@@ -20,15 +36,35 @@ export default function GistListing(props) {
   //   });
   // };
 
+  useEffect(()=> {
+    getListOfGists()
+  }, []) 
+
+  // useEffect(()=> {
+  //   console.log(gists)
+  //   gists.map(gist => console.log(gist.url))
+  //   console.log("useEffect po getListOfGists")
+  // }, [gists]) 
+
+
+
+// if(gists) {
   return (
     <div>
       {/* {gists.map((gist) => {
         return <div key={gist}></div>;
       })} */}
-      <button onClick={getListOfGists}>Get all new gists!</button>
+      <button type={"button"} onClick={getListOfGists}>Get all gists!</button>
       {gists.map((gist) => {
-         <div value={gist.files} key={gist.id} />;
+        return(
+          <div key={gist.id}>
+            <div>{gist.description}</div>
+            {/* <button type={"button"} onClick={deleteGist(gist.id)}>X</button> */}
+          </div>
+        )
       })}
+ 
     </div>
-  );
-}
+    );
+  }
+// }
