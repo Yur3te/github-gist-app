7f0fb692159db19dde649535db26fe1ba50404a1
Yur3te <Yur3te@gmail.com>
2021-08-27 21:00:56 +0200

    added function that makes appear list at first load of page

--- src/components/GistListing.jsx
+++ src/components/GistListing.jsx
@@ -1,70 +1,54 @@
 import React, { useState, useEffect } from "react";
 
-
 export default function GistListing(props) {
   const [gists, setGists] = useState([]);
-
-
+  // const [gistDeleted, setGistDeleted] = useState(false)
 
   const deleteGist = (id) => {
-    props.wrapper.deleteGist(id)
-    .then(response => {
-      console.log(response.setatus)
-        // if (response.status === 204) {
-        //     console.log("gist został usunięty", id)
-        // }
-    })
-}
-
-  
-  const getListOfGists = () => {
-    props.wrapper
-      .getAllGists()
-      .then((response) => {
-        setGists(response.data)
-      })
-      .catch(() => [])
-      
-      // .catch(() => []);
-    // setGists(res);
-    // console.log(gists)
-    console.log(gists.map((gist) => console.log(gist)));
+    props.wrapper.deleteGist(id).then((response) => {
+      if (response.status === 204) {
+        console.log("gist został usunięty", id);
+        // setGistDeleted(true)
+      } else console.log(response.setatus);
+    });
   };
-  // const downloadedGists = () => {
-  //   gists.map((gist) => {
-  //     return <div url={gist.html_url} key={gist.id} />;
-  //   });
-  // };
 
-  useEffect(()=> {
-    getListOfGists()
-  }, []) 
+  const getListOfGists =  () => {
+     props.wrapper
+    .getAllGists()
+    .then((response) => {
+      setGists(response.data);
+    })
+    .catch(() => [])
+    // console.log(gists.map((gist) => console.log(gist)));
+  };
 
-  // useEffect(()=> {
-  //   console.log(gists)
-  //   gists.map(gist => console.log(gist.url))
-  //   console.log("useEffect po getListOfGists")
-  // }, [gists]) 
+  useEffect(() => {
+    getListOfGists();
+  }, [props.tokenIsCorrect]);
 
+  // useEffect(()=>{
+  //   getListOfGists();
+  //   setGistDeleted(false)
+  // }, [gistDeleted])
 
-
-// if(gists) {
-  return (
-    <div>
-      {/* {gists.map((gist) => {
-        return <div key={gist}></div>;
-      })} */}
-      <button type={"button"} onClick={getListOfGists}>Get all gists!</button>
-      {gists.map((gist) => {
-        return(
-          <div key={gist.id}>
-            <div>{gist.description}</div>
-            <button type={"button"} onClick={() => deleteGist(gist.id)}>X</button>
-          </div>
-        )
-      })}
- 
-    </div>
+  if (gists) {
+    return (
+      <div>
+        <button type={"button"} onClick={getListOfGists}>
+          Refresh List!
+        </button>
+        {gists.map((gist) => {
+          return (
+            <div key={gist.id}>
+              <div>{gist.description}</div>
+              <button type={"button"} onClick={() => deleteGist(gist.id)}>
+                Remove gist
+              </button>
+            </div>
+          );
+        })}
+      </div>
     );
   }
-// }
+}
