338d65296b9936da0c668cbf560e2dcb3899ed44
Yur3te <Yur3te@gmail.com>
2021-08-28 21:25:33 +0200

    Added button to create 5 testing gists

--- src/components/AddGist.jsx
+++ src/components/AddGist.jsx
@@ -35,6 +35,32 @@ export default function AddGist(props) {
   const removeFile = (i) =>
     setFiles((files) => files.filter((value, index) => index !== i));
 
+  const createTestingGists = (event) => {
+    event.preventDefault();
+
+    let filesPayload = {
+        "hello_world.rb": {
+          content:
+            'class HelloWorld\n   def initialize(name)\n      @name = name.capitalize\n   end\n   def sayHi\n      puts "Hello !"\n   end\nend\n\nhello = HelloWorld.new("World")\nhello.sayHi',
+        },
+        "hello_world.py": {
+          content:
+            'class HelloWorld:\n\n    def __init__(self, name):\n        self.name = name.capitalize()\n       \n    def sayHi(self):\n        print "Hello " + self.name + "!"\n\nhello = HelloWorld("world")\nhello.sayHi()',
+        },
+        "hello_world_ruby.txt": {
+          content: "Run `ruby hello_world.rb` to print Hello World",
+        },
+        "hello_world_python.txt": {
+          content: "Run `python hello_world.py` to print Hello World",
+        },
+    };
+    for(let i = 0; i<5; i++){
+    props.wrapper
+      .createGist(`gist number ${i}`, false, filesPayload)
+      .then((response) => console.log(response));
+    }
+  };
+
   const create = (event) => {
     event.preventDefault();
 
@@ -64,6 +90,7 @@ export default function AddGist(props) {
 
   return (
     <div>
+      <button onClick={createTestingGists}>Create 5 gists for testing!</button>
       <div>Create new Gist!</div>
       <form onSubmit={create}>
         <div>
@@ -114,7 +141,7 @@ export default function AddGist(props) {
           />
         </div>
 
-        <input type="submit" value="Wyślij" />
+        <input type="submit" value="Submit" />
       </form>
     </div>
   );
